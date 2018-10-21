07beabacf2a73bf8ab77e527eb87f8b2ebed7fac
Mike Green <mike.is.green@gmail.com>
2018-10-21 14:54:46 -0400

    Add REPL init script to speed up interactive dev

---
+++ repl.js
@@ -0,0 +1,6 @@
+// Nesh REPL bootstrap script
+
+const data = require('./etc/project-sets.json');
+const ProjectSet = require('./lib/project-set');
+const Project = require('./lib/project');
+const { Interval, DateTime } = require('luxon');
